3d2e06b4b816595d795d45d964e1781a880ac079
mmarfinetz <82287267+mmarfinetz@users.noreply.github.com>
2025-04-07 11:03:04 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+
 export default defineConfig({
-	plugins: [react()],
-});
+  plugins: [react()],
+  base: '/salary_research/', // Add this line - must match your repo name
+  // other config options...
+})
